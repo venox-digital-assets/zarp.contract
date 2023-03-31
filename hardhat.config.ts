20ce208aa21f38345d1459f6a7400a7dccf9d9f2
Kenny Inggs <kenny.inggs@gmail.com>
2023-03-31 11:54:20 +0200

    Fixed error message

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,10 +1,10 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomiclabs/hardhat-etherscan";
-import "@nomicfoundation/hardhat-toolbox";
-import "@openzeppelin/hardhat-upgrades";
-import "@openzeppelin/hardhat-defender";
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomiclabs/hardhat-etherscan';
+import '@nomicfoundation/hardhat-toolbox';
+import '@openzeppelin/hardhat-upgrades';
+import '@openzeppelin/hardhat-defender';
 
-require("dotenv").config();
+require('dotenv').config();
 const {
   INFURA_API_KEY: apiKey,
   DEPLOYER_PRIVATE_KEY: deployerKey,
@@ -12,11 +12,11 @@ const {
   DEFENDER_API_KEY: defenderKey,
   DEFENDER_SECRET: defenderSecret,
 } = process.env;
-if (!apiKey) throw new Error("No INFURA_API_KEY in dotenv");
-if (!deployerKey) throw new Error("No GOERLI_PRIVATE_KEY in dotenv");
-if (!etherscanKey) throw new Error("No ETHERSCAN_API_KEY in dotenv");
-if (!defenderKey) throw new Error("No DEFENDER_API_KEY in dotenv");
-if (!defenderSecret) throw new Error("No DEFENDER_SECRET in dotenv");
+if (!apiKey) throw new Error('No INFURA_API_KEY in dotenv');
+if (!deployerKey) throw new Error('No DEPLOYER_PRIVATE_KEY in dotenv');
+if (!etherscanKey) throw new Error('No ETHERSCAN_API_KEY in dotenv');
+if (!defenderKey) throw new Error('No DEFENDER_API_KEY in dotenv');
+if (!defenderSecret) throw new Error('No DEFENDER_SECRET in dotenv');
 
 const config: HardhatUserConfig = {
   solidity: '0.8.18',
