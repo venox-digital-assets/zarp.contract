b8acdca4f6ec6b6733796ca1f8e53f1ff77e089b
Kenny Inggs <kinggs@users.noreply.github.com>
2023-04-03 12:42:53 +0200

    V3 upgrade (#2)
    * V3 upgrade
    - Solidity ^0.8.9
    - Hardhat (instead of truffle + waffle + ganache)
    - Upgradeable contract

--- migrations/1_initial_migration.js
+++
@@ -1,5 +0,0 @@
-const zarp = artifacts.require("ZARP");
-
-module.exports = function (deployer) {
-  deployer.deploy(zarp);
-};
---
+++ scripts/Upgrade.ts
@@ -0,0 +1,28 @@
+import { ethers, upgrades, defender } from 'hardhat';
+
+// Upgrade deployment of ZARP on a chain. Deploys new implementation and submits upgrade proposal on defender
+
+const UPGRADEABLE_ADDRESS = '0xb755506531786c8ac63b756bab1ac387bacb0c04'; // GOERLI address. Expecting same on mainnet at the moment
+const ZARP_REPO_URL = 'https://github.com/venox-digital-assets/zarp.contract';
+const APPROVAL_ADDRESS = '0xfA4EB9AA068B3b64348f42b142E270f28E2f86EB';
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+
+  console.log('Deploying contracts with the account:', deployer.address);
+
+  console.log('Account balance:', (await deployer.getBalance()).toString());
+
+  const token = await ethers.getContractFactory('Zarp');
+  const proposal = await defender.proposeUpgrade(UPGRADEABLE_ADDRESS, token, {
+    multisig: APPROVAL_ADDRESS,
+  });
+  console.log('Upgrade proposal created at:', proposal.url);
+  const verification = await defender.verifyDeployment(UPGRADEABLE_ADDRESS, 'Zarp', ZARP_REPO_URL);
+  console.log(`Verified artifact with hash`, verification.providedSha256);
+}
+
+main().catch(error => {
+  console.error(error);
+  process.exitCode = 1;
+});
